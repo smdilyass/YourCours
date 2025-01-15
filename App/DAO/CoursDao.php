1c2283024937a74815ff2f279236ab2c65da2078
smdilyass <smdilyass@gmail.com>
2025-01-15 10:58:04 +0100

    i creat CREAT methode in coursDAO file

--- App/DAO/CoursDao.php
+++ App/DAO/CoursDao.php
@@ -2,17 +2,24 @@
 
 class CoursDao{
     public function create(cours $cours):Cours{
-     if (empty($cours->getNom() || $cours->getNom() == null))
+     if (empty($cours->getNom()) || $cours->getNom() == null)
      {
         $cours->setNom("default Nom");
      }
-     if (empty($cours->getDescription() || $cours->getDescription() == null)){
+     if (empty($cours->getDescription()) || $cours->getDescription() == null)
+     {
         $cours->setDescription("default Description");
      }
-     if (empty($cours->getContenu() || $cours->getContenu() == null)){
-        $cours->setContenu("default Contenu")
+     if (empty($cours->getContenu()) || $cours->getContenu() == null){
+        $cours->setContenu("default Contenu");
+    }
+     if (empty($cours->getEnseignant()) || $cours->getEnseignant() == null)
+     {
+        $defaultEnseignant = new Enseignant();
+        $defaultEnseignant->setNom("default Enseignant");
+        $cours->setEnseignant($defaultEnseignant);
      }
-     return $cours
+     return $cours;
     }
 }
 
